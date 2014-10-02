22010de0f9c96f73edc07d000b0f868d1e1b6fec
jt222ic <jt222ic@student.lnu.se>
2014-10-02 15:41:44 +0200

    icke färdigt men är påväg , större förändring mellan variablen och användning av metoder, Lärde mig att använda try-catch med exeption argument inkluderad
    returnera värdet till 0 genom static Oddbyte
    
    Behöver en ny form till triangeln så man får inmatning av värden till triangelns bas.

--- 1dv402-jt222ic-1-2- asterisker B/Program.cs
+++ 1dv402-jt222ic-1-2- asterisker B/Program.cs
@@ -14,63 +14,77 @@ namespace _1dv402_jt222ic_1_2__asterisker_B
             do
             {
                 byte amount;
-                Console.Write("Ange det udda antalet asterisker <max 79> i triangelns bas: ");
-                amount = byte.Parse(Console.ReadLine());
-           
+                Console.Write("Ange det udda antalet asterisker <max 79> i triangelns bas: ");    ///anger variabeln
+                amount = ReadOddbyte();
+                RenderTriangle(amount);
+                //amount = byte.Parse(Console.ReadLine());
                 
+                //if (amount<= 79)
+                //{ RenderTriangle(amount); }
+                //if  (amount > 79)
+                //{ ReadOddbyte(amount); }
+                //if (amount % 1 == 0)
+                if (amount > 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("tryck tangent för att forsätta - ESC avslutar");
+                    Console.ResetColor();
 
-                Console.BackgroundColor = ConsoleColor.DarkGreen;                                  /// avslutad text  
-                Console.WriteLine("tryck tangent för att forsätta - ESC avslutar");           /// måste stoppas direkt 
-                                                                                              /// när jag skriver in värden som är
-                                                                                              /// över 79
-                Console.ResetColor();
+                }
+                
 
             }
 
             
 
-            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);   /// escape eller köra vidare med konsolen 
 
         }
 
-      
-
-        static void ReadOddByte(byte oddOrEven)
+        static byte ReadOddbyte()
         {
-           
-
-  
+            byte amount;
             try
             {
-             if ( oddOrEven > 79)
-	       {                                  
-        throw new ArgumentException("Number is not allowed to be over 79");
-	        } 
-             else if ( oddOrEven % 2 == 0)
-             {
-                 Console.WriteLine("Hey babariba");
+                
 
-             }
 
+                Console.ResetColor();
+                amount = byte.Parse(Console.ReadLine());     //// skriva in icke udda tal 
+                                                           //// execption kastar sig till argument
+                                                           /// catchar andra fel utan tal
+                if (amount % 2 == 0)
+                { throw new ArgumentException(); }
+                if (amount>79)
+                { throw new ArgumentException(); }
+                else
+                return amount;
             }
-            catch(Exception e)                                                                          /// här ska jag använda mig av 
-                                                                                           /// try catch formen
-                                                                                           /// för att bestämma om värden är
-                                                                                           /// udda eller lika, fel meddelandet
-                                                                                           /// ska uppstå. 
+            catch (ArgumentException)
             {
-                Console.WriteLine("e.Message"); 
-                    
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fel! den inmatade värden är inte ett udda heltal mellan 1 och 79.");
+                Console.ResetColor();
+                return 0;
             }
-            return;
-        }
+            catch 
+                
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fel! den inmatade värden är inte ett udda heltal mellan 1 och 79. ");
+                Console.ResetColor();
+            return 0;
+            }
+        }     
+
+        
        static void RenderTriangle(byte cols)
         {
             
-           for (int i = 1; i <= cols; i++)                  ////Antal nr skrivs in = cols
+           for (int i = 1; i <= cols; i++)      ///höjd               ////Antal nr skrivs in = cols
            {
 
-               for (int j = i; j <= cols; j++)         /////TRiangelns metods räkning
+               for (int j = i; j <= cols; j++)    ///pusha stjärnorna på toppen      /////TRiangelns metods räkning
                {
                    Console.Write("  ");                ///// mellanrum
 
@@ -82,7 +96,6 @@ namespace _1dv402_jt222ic_1_2__asterisker_B
                }
                Console.WriteLine();
            }
-           
 
 
 
